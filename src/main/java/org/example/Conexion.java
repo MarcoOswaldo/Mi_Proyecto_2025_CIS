d998547de3051ce6e06b9fc429c0420a79801094
MarcoOswaldo <marcotellosn@gmail.com>
2025-07-20 19:00:09 -0500

    Update Conexion.java

--- src/main/java/org/example/Conexion.java
+++ src/main/java/org/example/Conexion.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/unidad_cancer?serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String CLAVE = "marco@1234T";
+    private static final String CLAVE = "password";
 
     public static Connection obtenerConexion() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, CLAVE);
